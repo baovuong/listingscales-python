c206b40ae6957e95932890d92fe7792e76cd80c1
Bao Vuong <vqbao@lv5.org>
2019-12-31 11:18:16 -0500

    got webpack and sass to work together

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
- var webpack = require('webpack');  
+var webpack = require('webpack');  
 var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     entry: ['./js/app.js'],
     devtool: 'sourcemaps',
     cache: true,
@@ -22,16 +24,41 @@ module.exports = {
                 }
             },
             {
-                test: /\.s[ac]ss$/i,
+                // Apply rule for .sass, .scss or .css files
+                test: /\.(sa|sc|c)ss$/,
+          
+                // Set loaders to transform files.
+                // Loaders are applying from right to left(!)
+                // The first loader will be applied after others
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                       {
+                         // This loader resolves url() and @imports inside CSS
+                         loader: "css-loader",
+                       },
+                       {
+                         // Then we apply postCSS fixes like autoprefixer and minifying
+                         loader: "postcss-loader"
+                       },
+                       {
+                         // First we transform SASS to standard CSS
+                         loader: "sass-loader",
+                         options: {
+                           implementation: require("sass")
+                         }
+                       }
+                     ]
+              }
         ],
         
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ],
     resolve: {
         modules: [path.resolve(__dirname, "./js"), "node_modules"],
         extensions: ['.js', '.jsx']
